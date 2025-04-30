533afa2905f1e10dce955801bce5b9fcf269b96a
Adibul Asam <0432310005101025@uits.edu.bd>
2025-04-30 23:03:25 +0600

    ..

--- Templates/boilerPlate.cpp
+++ Templates/boilerPlate.cpp
@@ -1,3 +1,4 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -9,7 +10,10 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    
+    int t; cin >> t;
+    while(t--) {
+        
+    }
 
 
     return 0;
